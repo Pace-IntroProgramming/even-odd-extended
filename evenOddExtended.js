c91dbc51d4d299fe8b3696d0dc7f7f8437c4a089
John Pearson <john.pearson@pacecademy.org>
2017-10-04 11:31:56 -0400

    Created student version.

--- evenOddExtended.js
+++ evenOddExtended.js
@@ -1,22 +1,22 @@
 // evenOddExtended takes an input n and determines if it is an even number
 function evenOddExtended(n) {
     // First remind the user what number they entered
-    console.log("You entered '" + n + "'.");
+    console.log();
 
     // Next determine if the input is actually an integer
-    if (parseInt(n) == n) {
+    if () {
         // If the input is an integer, determine if it is positive
-        if (n > 0) {
+        if () {
             // If the input is a positive integer, determine if it is even
-            if (n % 2 == 0) {
-                console.log('Your number is even!');
+            if () {
+                console.log();
             } else {
-                console.log('Your number is odd!');
+                console.log();
             }
         } else {
-            console.log('Your number is not a positive number.');
+            console.log();
         }
     } else {
-        console.log('Your input is not an integer.');
+        console.log();
     }
 }
