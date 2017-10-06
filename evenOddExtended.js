570ccefb668d222518f0390dbde9dd7a80512da9
John Pearson <john.pearson@pacecademy.org>
2017-10-06 11:41:22 -0400

    Added testing.

--- evenOddExtended.js
+++ evenOddExtended.js
@@ -20,3 +20,43 @@ function evenOddExtended(n) {
         console.log();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------
+// Testing - DON'T CHANGE ANYTHING BELOW
+const printme = console.log;
+let out = [];
+console.log = function log(d) {
+    out.push(d);
+};
+evenOddExtended(2);
+printme('Testing...');
+printme(`\t did you properly return the user input?\t ${out[0] == 'You entered 2.'} `);
+const a = [3.1, -3, 3, 4];
+for (let i = 0; i < a.length; i += 1) {
+    out = [];
+    evenOddExtended(a[i]);
+    const t = a[i] % 2 == 0 ? 'even' : 'odd';
+    if(parseInt(a[i]) != a[i]) {
+        printme(`\t did you properly return a statement for non-integer values?\t ${out[1] == 'Your input is not an integer.'}`);
+    } else if(a[i] < 0) {
+        printme(`\t did you properly return a statement for negative values?\t ${out[1] == 'Your number is not a positive number.'}`);
+    } else {
+        printme(`\t did you properly return a statement for ${t} values?\t ${out[1] == 'Your number is ' + t + '!'}`);
+    }
+}
