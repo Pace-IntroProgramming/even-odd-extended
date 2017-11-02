d93dd43574bd3d3620d8b3856a200adac21b942f
gwpjp <john.pearson.02@gmail.com>
2017-11-02 11:13:17 -0400

    Refactored code.

--- evenOddExtended.js
+++ evenOddExtended.js
@@ -52,9 +52,9 @@ for (let i = 0; i < a.length; i += 1) {
     out = [];
     evenOddExtended(a[i]);
     const t = a[i] % 2 == 0 ? 'even' : 'odd';
-    if(parseInt(a[i]) != a[i]) {
+    if (parseInt(a[i]) != a[i]) {
         printme(`\t did you properly return a statement for non-integer values?\t ${out[1] == 'Your input is not an integer.'}`);
-    } else if(a[i] < 0) {
+    } else if (a[i] < 0) {
         printme(`\t did you properly return a statement for negative values?\t ${out[1] == 'Your number is not a positive number.'}`);
     } else {
         printme(`\t did you properly return a statement for ${t} values?\t ${out[1] == 'Your number is ' + t + '!'}`);
